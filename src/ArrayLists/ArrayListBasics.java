ecd2c2d5e76602430344923fd84c5fb28c01eaf4
malik-kamran-mushtaq <kamran145422@gmail.com>
2023-12-20 15:17:57 +0530

    array list basics

---
+++ src/ArrayLists/ArrayListBasics.java
@@ -0,0 +1,29 @@
+package ArrayLists;
+
+import java.util.ArrayList;
+
+public class ArrayListBasics {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        System.out.println(list);
+        System.out.println(list.get(3));
+        System.out.println(list.contains(5));
+
+        list.remove(5);
+        System.out.println(list);
+
+        System.out.println(list.size());
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ src/ArrayLists/ReverseArrayList.java
@@ -0,0 +1,2 @@
+package ArrayLists;public class ReverseArrayList {
+}
