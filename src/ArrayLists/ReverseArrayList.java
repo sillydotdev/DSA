2571a4d6358010706deeee7b0c9637138bada277
malik-kamran-mushtaq <kamran145422@gmail.com>
2023-12-20 15:18:36 +0530

    reverse array list

--- src/ArrayLists/ReverseArrayList.java
+++ src/ArrayLists/ReverseArrayList.java
@@ -1,2 +1,22 @@
-package ArrayLists;public class ReverseArrayList {
+package ArrayLists;
+
+import java.util.ArrayList;
+
+public class ReverseArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        System.out.println(list.reversed());
+
+        for (int i = list.size()-1; i >= 0 ; i--) {
+            System.out.print(list.get(i)+" ");
+        }
+        System.out.println();
+    }
 }
