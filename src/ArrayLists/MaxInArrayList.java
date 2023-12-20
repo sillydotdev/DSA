c1f218f120999df3c86e1953dd29eb6d6bc8d517
malik-kamran-mushtaq <kamran145422@gmail.com>
2023-12-20 15:19:07 +0530

    max in an arraylit array list

--- src/ArrayLists/MaxInArrayList.java
+++ src/ArrayLists/MaxInArrayList.java
@@ -1,2 +1,26 @@
-package ArrayLists;public class MaxInArrayList {
+package ArrayLists;
+
+import java.util.ArrayList;
+
+public class MaxInArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < list.size(); i++) {
+//            if(max < list.get(i)) {       // one way
+//                max = list.get(i);
+//            }
+            max = Math.max(max, list.get(i));   // another way
+        }
+        System.out.println("Max Element = "+max);
+
+    }
 }
