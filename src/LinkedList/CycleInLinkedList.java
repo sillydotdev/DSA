136ac756b935f8587884337e323ce0d9de6f883c
malik-kamran-mushtaq <kamran145422@gmail.com>
2023-12-25 12:59:14 +0530

    cycle in a linkedList

---
+++ src/LinkedList/CycleInLinkedList.java
@@ -0,0 +1,40 @@
+// check whether there is a cycle in a LinkedList or not
+// FLOYD's cycle finding algorithm
+package LinkedList;
+
+public class CycleInLinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+
+    public static boolean isCycle() {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static  void main(String[] args) {
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = head;
+
+        System.out.println(isCycle());
+
+    }
+}
