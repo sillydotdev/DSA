b669d121d712437ccb295dc10a5b52e4c17d9790
malik-kamran-mushtaq <kamran145422@gmail.com>
2024-01-01 19:26:05 +0530

    First Non-Repeating letter in a stream of chracaters

---
+++ src/Queues/FirstNonRepeating.java
@@ -0,0 +1,30 @@
+package Queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FirstNonRepeating {
+    public static void firstNonRepeating(String str) {
+        int freq[] = new int[26];   // array to store frequency of each character
+        Queue<Character> q = new LinkedList<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            q.add(ch);
+            freq[ch - 'a']++;
+
+            while (!q.isEmpty() && freq[q.peek() - 'a'] > 1) {
+                q.remove();
+            }
+            if (q.isEmpty()) {
+                System.out.print(-1+" ");
+            } else {
+                System.out.print(q.peek()+" ");
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        String str = "aabccxb";
+        firstNonRepeating(str);
+    }
+}
