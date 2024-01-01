d63cd17b646561965e1f8d295a6543f1deda7b97
malik-kamran-mushtaq <kamran145422@gmail.com>
2024-01-01 15:39:16 +0530

    Queue Using 2 Stacks

--- src/Queues/QueueUsingTwoStacks.java
+++ src/Queues/QueueUsingTwoStacks.java
@@ -46,6 +46,7 @@ public class QueueUsingTwoStacks {
             q.add(1);
             q.add(2);
             q.add(3);
+            q.add(4);
 
             while (!q.isEmpty()) {
                 System.out.println(q.peek());
