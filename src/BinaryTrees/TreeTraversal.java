5f8fde33e23ddb6077ad2fd4b1c0a021153a37a8
malik-kamran-mushtaq <kamran145422@gmail.com>
2024-01-07 14:43:33 +0530

    Tree Traversals - PreOrder, InOrder & PostOrder

--- src/BinaryTrees/TreeTraversal.java
+++ src/BinaryTrees/TreeTraversal.java
@@ -1,6 +1,6 @@
 package BinaryTrees;
 
-public class PreOrder {
+public class TreeTraversal {
     static class Node {
         int data;
         Node left;
@@ -26,6 +26,7 @@ public class PreOrder {
             return newNode;
         }
 
+        // PreOrder Traversal
         public static void preOrder(Node root) {
             if (root == null) {
                 return;
@@ -36,12 +37,36 @@ public class PreOrder {
         }
 
 
+        // InOrder Traversal
+        public static void inOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+            inOrder(root.left);
+            System.out.print(root.data+" ");
+            inOrder(root.right);
+        }
+
+        //PostOrder Traversal
+        public static void postOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data+" ");
+        }
     }
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
 
+        System.out.println();
         tree.preOrder(root);
+        System.out.println();
+        tree.inOrder(root);
+        System.out.println();
+        tree.postOrder(root);
     }
 }
