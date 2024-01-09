6a7ed01251e46e58cdaf3d3c0c187d07534568ea
malik-kamran-mushtaq <kamran145422@gmail.com>
2024-01-09 17:29:51 +0530

    Print nodes of Kth level of a tree

---
+++ src/BinaryTrees/KthLevel.java
@@ -0,0 +1,40 @@
+package BinaryTrees;
+
+public class KthLevel {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // Print Kth level
+    public static void kthLevel(Node root, int level, int k) {
+        if (root == null) {
+            return;
+        }
+        if (level == k) {
+            System.out.print(root.data+" ");
+            return;
+        }
+        kthLevel(root.left, level + 1, k);
+        kthLevel(root.right, level + 1, k);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int k = 2;
+        kthLevel(root, 1, k);
+    }
+}
