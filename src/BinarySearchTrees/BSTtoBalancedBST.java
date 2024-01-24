748f1e9172ea7cb92ebd3ee214e69d72784886a3
malik-kamran-mushtaq <kamran145422@gmail.com>
2024-01-24 17:59:26 +0530

    convert BST to Balanced BST

--- src/BinarySearchTrees/BSTtoBalancedBST.java
+++ src/BinarySearchTrees/BSTtoBalancedBST.java
@@ -1,2 +1,68 @@
-package BinarySearchTrees;public class BSTtoBalancedBST {
+package BinarySearchTrees;
+
+import java.util.ArrayList;
+
+public class BSTtoBalancedBST {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+        public Node(int data) {
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
+
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    // get inorder
+    public static void getInorder(Node root, ArrayList<Integer> inorder) {
+        if (root == null) {
+            return;
+        }
+        getInorder(root.left, inorder);
+        inorder.add(root.data);
+        getInorder(root.right, inorder);
+    }
+
+    // convert inorder to balanced BST
+    public static Node createBST(ArrayList<Integer> inorder, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int mid = start + (end - start) / 2;
+        Node root = new Node(inorder.get(mid));
+        root.left = createBST(inorder, start, mid-1);
+        root.right = createBST(inorder, mid+1, end);
+        return root;
+    }
+    // convert BST to Balanced BST
+    public static Node balancedBST(Node root) {
+        // inorder sequence
+        ArrayList<Integer> inorder = new ArrayList<>();
+        getInorder(root, inorder);
+        // convert BST to Balanced BST
+        root = createBST(inorder, 0, inorder.size()-1);
+        return root;
+    }
+    public static void main(String[] args) {
+        Node root = new Node(8);
+        root.left = new Node(6);
+        root.left.left = new Node(5);
+        root.left.left.left = new Node(3);
+
+        root.right = new Node(10);
+        root.right.right = new Node(11);
+        root.right.right.right = new Node(12);
+
+        root = balancedBST(root);
+        preorder(root);
+    }
 }
