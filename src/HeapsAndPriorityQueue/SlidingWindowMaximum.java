912bf50bc03352c053ce5fb0db2527fa782bd601
malik-kamran-mushtaq <kamran145422@gmail.com>
2024-02-01 16:23:25 +0530

    Sliding window Maximum

--- src/HeapsAndPriorityQueue/SlidingWindowMaximum.java
+++ src/HeapsAndPriorityQueue/SlidingWindowMaximum.java
@@ -1,2 +1,11 @@
-package HeapsAndPriorityQueue;public class SlidingWindowMaximum {
+package HeapsAndPriorityQueue;
+
+public class SlidingWindowMaximum {
+    public static void main(String[] args) {
+        int arr[] = {2, 4, 2, 5, 35, 22, 5};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
 }
