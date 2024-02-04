b2abef41e5bb9549a2c10db84d7d7334466b104a
malik-kamran-mushtaq <kamran145422@gmail.com>
2024-02-04 08:59:39 +0530

    Sliding window maximum

--- src/HeapsAndPriorityQueue/SlidingWindowMaximum.java
+++ src/HeapsAndPriorityQueue/SlidingWindowMaximum.java
@@ -1,15 +1,47 @@
 package HeapsAndPriorityQueue;
 
-public class SlidingWindowMaximum {
-    public static void main(String[] args) {
-        int arr[] = {2, 4, 2, 5, 35, 22, 5};
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
+import java.util.PriorityQueue;
 
-        int a = 10, b = 90;
-        int c = a + b;
-        System.out.println(c);
+public class SlidingWindowMaximum {
+    static class Pair implements Comparable<Pair>{
+        int value;
+        int index;
+        public Pair(int value, int index) {
+            this.value = value;
+            this.index = index;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+            // ascending order
+//            return this.number - p2.number;
+            // descending order
+            return p2.value - this.value;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+        int res[] = new int[arr.length-k+1];
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < k; i++) {
+            pq.add(new Pair(arr[i], i));
+        }
+
+        res[0] = pq.peek().value;
+
+        for (int i = k; i < arr.length; i++) {
+            while (pq.size() > 0 && pq.peek().index <= (i-k)) {
+                pq.remove();
+            }
+            pq.add(new Pair(arr[i], i));
+            res[i-k+1] = pq.peek().value;
+        }
+
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i]+" ");
+        }
     }
 }
