20ec49264dcc25c5905248d440ca79cc622405da
Malik Kamran Mushtaq <kamran145422@gmail.com>
2024-03-22 07:07:55 +0530

    power exponentiation

--- src/Maths/PowerExponentiation.java
+++ src/Maths/PowerExponentiation.java
@@ -1,4 +1,22 @@
 package Maths;
 
 public class PowerExponentiation {
+    public static int powerExponentiation(int x, int n) {
+        int temp = n;   // for negative n
+        int ans = 1;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                ans = ans * x;
+                n = n - 1;
+            } else {
+                n = n / 2;
+                x = x * x;
+            }
+        }
+        if (temp < 0) ans = 1 / ans;    // for negative n
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.println(powerExponentiation(2, 21));
+    }
 }
